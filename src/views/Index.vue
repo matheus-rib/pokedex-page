2faefec338b38217e08e8f41935f0cbbc8253c0f
Matheus Ribeiro <matt.ribeiro14@outlook.com>
2020-10-01 22:55:49 -0300

    wip: Index page for regions list

--- src/views/Index.vue
+++ src/views/Index.vue
@@ -1,3 +1,21 @@
 <template lang="pug">
-div Teste
+div {{regionsList}}
 </template>
+
+<script>
+import { mapState, mapActions } from "vuex"
+
+export default {
+  computed: {
+    ...mapState('regions', ['regionsList']),
+  },
+
+  methods: {
+    ...mapActions('regions', ['fetchRegionsList']),
+  },
+
+  async created() {
+    await this.fetchRegionsList()
+  },
+}
+</script>
