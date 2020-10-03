3c12ab66f7db2f8308df03aa37d3947aeedc9100
Matheus Ribeiro <matt.ribeiro14@outlook.com>
2020-10-02 23:10:42 -0300

    Add filterField component

---
+++ src/components/FilterField.vue
@@ -0,0 +1,37 @@
+<template lang="pug">
+form.has-icon-right(@submit.prevent="search")
+  input.form-input.input-sm(:placeholder="placeholder" v-model="searchInputModel")
+  i.form-icon.fas.fa-search.c-hand(@click="search")
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      searchInputModel: ''
+    }
+  },
+
+  props: {
+    placeholder: {
+      type: String,
+      default: 'Pesquisar'
+    }
+  },
+
+  methods: {
+    search() {
+      this.$emit('search', this.searchInputModel)
+    },
+  },
+}
+</script>
+
+<style lang="stylus" scoped>
+input.form-input.input-sm
+  border-radius 100px
+  padding-left 10px
+
+i.form-icon.fas.fa-search
+  right .3rem
+</style>
