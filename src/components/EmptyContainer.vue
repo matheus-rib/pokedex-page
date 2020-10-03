feaed66822bac51433c55965c37126490d8b9aab
Matheus Ribeiro <matt.ribeiro14@outlook.com>
2020-10-03 03:49:52 -0300

    Add EmptyContainer component

---
+++ src/components/EmptyContainer.vue
@@ -0,0 +1,34 @@
+<template lang="pug">
+.empty
+  .empty-icon(v-if="icon")
+    i(:class="icon")
+  p.empty-title.h5 {{title}}
+  p.empty-subtitle {{subtitle}}
+  slot(name="action")
+</template>
+
+<script>
+export default {
+  props: {
+    icon: {
+      type: String,
+      default: 'fas fa-times fa-5x color-danger',
+    },
+
+    title: {
+      type: String,
+      default: 'Sem resultados',
+    },
+
+    subtitle: {
+      type: String,
+      default: 'Não encontrado resultados para essa busca.',
+    },
+  },
+}
+</script>
+
+<style lang="stylus" scoped>
+.color-danger
+  color #ff0000
+</style>
