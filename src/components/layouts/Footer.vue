7b04d751eebb280aa5bc6c93763af880ddb24035
Matheus Ribeiro <matt.ribeiro14@outlook.com>
2020-10-04 00:18:56 -0300

    Add footer component

---
+++ src/components/layouts/Footer.vue
@@ -0,0 +1,24 @@
+<template lang="pug">
+.text-ellipsis
+  .divider
+  footer.text-dark
+    a(href="https://github.com/matheus-rib/pokedex-page") Pokedex-page
+    code
+      small (v{{version}})
+</template>
+
+<script>
+import { version } from '../../../package'
+export default {
+  data() {
+    return {
+      version,
+    }
+  },
+}
+</script>
+
+<style lang="stylus" scoped>
+.text-dark
+  text-align right
+</style>
