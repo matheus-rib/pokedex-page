b6e27a5c84fe6a692e99bdb5f77480ec807ca1ea
Matheus Ribeiro <matt.ribeiro14@outlook.com>
2020-10-04 00:19:10 -0300

    Fixed header import to the new path and add footer component

--- src/App.vue
+++ src/App.vue
@@ -2,12 +2,22 @@
 #app
   header-component
   router-view
+  .footer
+    footer-component
 </template>
 
 <script>
-import HeaderComponent from '@/components/layouts/Header'
+import HeaderComponent from '@/components/layouts/header/Index'
+import FooterComponent from '@/components/layouts/Footer';
 
 export default {
-  components: { HeaderComponent }
+  components: { HeaderComponent, FooterComponent }
 }
 </script>
+
+<style lang="stylus" scoped>
+.footer
+  position fixed
+  bottom 0
+  width 100%
+</style>
