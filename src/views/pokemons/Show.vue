ab443b21ce96957b888e9c6b3ac4800abe96cebc
Matheus Ribeiro <matt.ribeiro14@outlook.com>
2020-10-11 00:45:45 -0300

    Add empty-container displaying error when fails to fetch pokemon

--- src/views/pokemons/Show.vue
+++ src/views/pokemons/Show.vue
@@ -1,6 +1,6 @@
 <template lang="pug">
 .container.grid-xl(v-if="!loading")
-  .pokemon-data
+  .pokemon-data(v-if="!error")
     img.pokemon-portrait(v-if="currentPokemon.sprites.front_default" :src="currentPokemon.sprites.front_default")
     .pokemon-portrait.empty(v-else)
       i.fas.fa-camera.fa-4x.fa-fw
@@ -9,26 +9,42 @@
       .text-size Name: {{currentPokemon.name | capitalize}}
       .text-size Type(s): 
         span.chip(v-for="type in currentPokemon.types" :key="type.slot" :style="{background: pokemonTypes[type.type.name].color}") {{pokemonTypes[type.type.name].name}}
+  div(v-else)
+    empty-container(:title="errorMessage" :subtitle="error")
+      button.btn.btn-primary(slot="action" @click="goBack") Go back
 .loading.loading-lg(v-else)
 </template>
 
 <script>
 import { mapState, mapActions } from 'vuex'
 import pokemonTypes from '@/lib/PokemonTypes'
+import EmptyContainer from '@/components/EmptyContainer'
+import { ucfirst } from 'paliari-js-utils';
 export default {
   data() {
     return {
       loading: false,
       pokemonTypes,
+      error: null,
     }
   },
 
+  components: { EmptyContainer },
+
   computed: {
     ...mapState('pokemons', ['currentPokemon']),
+
+    errorMessage() {
+      return `Error fetching: ${ucfirst(this.$route.params.pokemonName)}`
+    },
   },
 
   methods: {
     ...mapActions('pokemons', ['fetchPokemon']),
+
+    goBack() {
+      this.$router.go(-1)
+    },
   },
 
   async created () {
@@ -37,6 +53,7 @@ export default {
       await this.fetchPokemon(this.$route.params.pokemonName)
     } catch (e) {
       this.$toaster.error(e.message)
+      this.error = e.message
     } finally {
       this.loading = false
     }
